fcad5fa8349ce0f49874e4c9df5e39159ef57e87
Mikalay Hancharonok <classic76@tut.by>
2022-08-12 10:57:36 +0300

    feat: task complite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+    return matrix.map((x, i) => (i % 2 === 0 ? x : x.reverse())).flat();
+};
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ];
+// towelSort(matrix);
